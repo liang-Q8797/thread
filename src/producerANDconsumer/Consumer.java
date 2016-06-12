7abeaeddbc9854fbc9adf406418108809b785653
liangshuai0222 <liangshuai0222@126.com>
2016-06-12 17:05:15 +0800

    解决线程池关闭，线程没有被中断。

--- src/producerANDconsumer/Consumer.java
+++ src/producerANDconsumer/Consumer.java
@@ -3,6 +3,7 @@ package producerANDconsumer;
 import java.text.MessageFormat;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class Consumer implements Runnable{
 	private BlockingQueue<PCData> queue;
@@ -14,13 +15,19 @@ public class Consumer implements Runnable{
 	@Override
 	public void run() {
 		try{
-			while(true){
-				PCData data = queue.take();
+			while(true)
+			{
+				//PCData data = queue.take(); //如果没有数据，一直等待
+				//超过50毫秒，则返回null
+				PCData data = queue.poll(1000,TimeUnit.MILLISECONDS);
 				if(data != null){
 					System.out.println("消费者："+Thread.currentThread().getName()+": "+data);
 					int result = data.getData() * data.getData();
 					System.out.println(MessageFormat.format("{0}*{1}",data.getData(),data.getData(),result));
 					Thread.sleep(1000+new Random().nextInt(200));
+				}else{
+					System.out.println("没有数据可以处理了！");
+					break;
 				}
 			}
 		}catch(Exception e){
--- src/producerANDconsumer/Main.java
+++ src/producerANDconsumer/Main.java
@@ -9,29 +9,38 @@ public class Main {
 	public static void main(String[] args) {
 		BlockingQueue<PCData> queue = new LinkedBlockingQueue<>();
 		Producer producer = new Producer(queue);
-		Consumer consumer = new Consumer(queue); 
-//		Producer producer1 = new Producer(queue);
-//		Producer producer2 = new Producer(queue);
-//		Producer producer3 = new Producer(queue);
-//		Consumer Consumer1 = new Consumer(queue);
-//		Consumer Consumer2 = new Consumer(queue);
-//		Consumer Consumer3 = new Consumer(queue);
+		Consumer consumer = new Consumer(queue);
+		// Producer producer1 = new Producer(queue);
+		// Producer producer2 = new Producer(queue);
+		// Producer producer3 = new Producer(queue);
+		// Consumer Consumer1 = new Consumer(queue);
+		// Consumer Consumer2 = new Consumer(queue);
+		// Consumer Consumer3 = new Consumer(queue);
 		ExecutorService service = Executors.newFixedThreadPool(4);
-		for(int i=0;i<5;i++){
+		for (int i = 0; i < 5; i++) {
 			service.submit(producer);
 			service.submit(consumer);
 		}
-//		producer.stop();
-//		service.submit(producer1);
-//		service.submit(producer2);
-//		service.submit(producer3);
-//		
-//		service.submit(Consumer1);
-//		service.submit(Consumer2);
-//		service.submit(Consumer3);
-//		producer1.stop();
-//		producer2.stop();
-//		producer3.stop();
+		// producer.stop();
+		// service.submit(producer1);
+		// service.submit(producer2);
+		// service.submit(producer3);
+		//
+		// service.submit(Consumer1);
+		// service.submit(Consumer2);
+		// service.submit(Consumer3);
+		// producer1.stop();
+		// producer2.stop();
+		// producer3.stop();
 		service.shutdown();
+		while (!service.isTerminated()) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Finished all threads");
 	}
 }
